50db56440c3c126ced8f31c3969a0e9be2acf651
Bach Dao <ngocbach12a1@gmail.com>
2024-12-31 22:45:28 +0700

    Index Routes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,8 @@ const Header = () => {
     return (
         <Navbar expand="lg" bg="light">
             <Container>
-                <Navbar.Brand href="/">IT Zui Zẻ</Navbar.Brand>
+                {/* <Navbar.Brand href="/">IT Zui Zẻ</Navbar.Brand> */}
+                <NavLink to="/" className='navbar-brand'>IT Zui Zẻ</NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
@@ -18,11 +19,11 @@ const Header = () => {
                     </Nav>
                     <Nav>
                         <NavDropdown title="Setting" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Login</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
+                            <NavDropdown.Item >Login</NavDropdown.Item>
+                            <NavDropdown.Item >
                                 Log out
                             </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
+                            <NavDropdown.Item >Profile</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
