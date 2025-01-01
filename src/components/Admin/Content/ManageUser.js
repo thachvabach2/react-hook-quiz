c61fa5a789d4051a71c591dffb69c4a526712414
Bach Dao <ngocbach12a1@gmail.com>
2025-01-01 16:36:23 +0700

    State Hóa Modal Add New User

--- src/components/Admin/Content/ManageUser.js
+++ src/components/Admin/Content/ManageUser.js
@@ -1,5 +1,5 @@
 import ModalCreateUser from './ModalCreateUser'
-
+import './ManageUser.scss'
 
 const ManageUser = (props) => {
     return (
@@ -13,8 +13,8 @@ const ManageUser = (props) => {
                 </div>
                 <div>
                     table user
-                    <ModalCreateUser />
                 </div>
+                <ModalCreateUser />
             </div>
         </div>
     )
