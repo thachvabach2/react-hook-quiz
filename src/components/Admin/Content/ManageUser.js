573bfb9b150301f9bd75f60d3054001874b3fcbc
Bach Dao <ngocbach12a1@gmail.com>
2025-01-02 10:33:49 +0700

    Cập Nhật Danh Sách Users Khi Thêm Mới Thành Công

--- src/components/Admin/Content/ManageUser.js
+++ src/components/Admin/Content/ManageUser.js
@@ -1,12 +1,25 @@
 import ModalCreateUser from './ModalCreateUser'
 import './ManageUser.scss'
 import { FcPlus } from "react-icons/fc";
-import { useState } from 'react';
 import TableUser from './TableUser';
+import { useEffect, useState } from 'react'
+import { getAllUsers } from '../../../services/apiService'
 
 const ManageUser = (props) => {
 
     const [showModalCreateUser, setShowModalCreateUser] = useState(false)
+    const [listUsers, setListUsers] = useState([])
+
+    useEffect(() => {
+        fetchListUsers()
+    }, [])
+
+    const fetchListUsers = async () => {
+        let res = await getAllUsers()
+        if (res.EC === 0) {
+            setListUsers(res.DT)
+        }
+    }
 
     return (
         <div className='manage-user-container'>
@@ -22,11 +35,12 @@ const ManageUser = (props) => {
                     </button>
                 </div>
                 <div className='table-user-container'>
-                    <TableUser />
+                    <TableUser listUsers={listUsers} />
                 </div>
                 <ModalCreateUser
                     show={showModalCreateUser}
                     setShow={setShowModalCreateUser}
+                    fetchListUsers={fetchListUsers}
                 />
             </div>
         </div>
