7e4583f46100835f9782170805028b334cbd712c
Bach Dao <ngocbach12a1@gmail.com>
2025-01-08 20:10:23 +0700

    Process Data - Xử Lý Data Phía Frontend

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useParams } from "react-router-dom";
 import { getDataQuiz } from '../../services/apiService';
+import _ from 'lodash'
 
 const DetailQuiz = (props) => {
     const params = useParams()
@@ -12,7 +13,28 @@ const DetailQuiz = (props) => {
 
     const fetchQuestions = async () => {
         let res = await getDataQuiz(quizId)
-        console.log(res)
+        console.log('>>>> check question: ', res)
+        if (res && res.EC === 0) {
+            let raw = res.DT
+            let data = _.chain(raw)
+                // Group the elements of Array based on `color` property
+                .groupBy("id")
+                // `key` is group's name (color), `value` is the array of objects
+                .map((value, key) => {
+                    let questionDescription, image = null
+                    let answers = []
+                    answers = value.map((item, index) => {
+                        if (index === 0) {
+                            questionDescription = item.description
+                            image = item.image
+                        }
+                        return item.answers
+                    })
+                    return { questionId: key, answers, questionDescription, image }
+                })
+                .value()
+            console.log(data)
+        }
     }
 
     return (
