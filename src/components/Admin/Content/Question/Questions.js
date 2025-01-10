cb3e4099ae8326c6bed430996765292cb9b7de98
Bach Dao <ngocbach12a1@gmail.com>
2025-01-10 22:51:57 +0700

    Tạo Fake Data Cho Giao Diện

--- src/components/Admin/Content/Question/Questions.js
+++ src/components/Admin/Content/Question/Questions.js
@@ -3,6 +3,9 @@ import Select from 'react-select';
 import './Questions.scss'
 import { FaPlusCircle, FaMinusCircle } from "react-icons/fa";
 import { FaRegPlusSquare, FaRegMinusSquare } from "react-icons/fa";
+import { RiImageAddFill } from "react-icons/ri";
+import { v4 as uuidv4 } from 'uuid';
+import _ from 'lodash'
 
 const Questions = (props) => {
     const options = [
@@ -13,62 +16,161 @@ const Questions = (props) => {
 
     const [selectedQuiz, setSelectedQuiz] = useState({})
 
+    const handleAddRemoveQuestion = (type, id) => {
+        if (type === 'ADD') {
+            const newQuestion = {
+                id: uuidv4(),
+                description: '',
+                image: '',
+                imageFile: '',
+                imageName: '',
+                answers: [
+                    {
+                        id: uuidv4(),
+                        description: '',
+                        isCorrect: false
+                    }
+                ]
+            }
+            setQuestions([...questions, newQuestion])
+
+        }
+
+        if (type === 'REMOVE') {
+            let questionClone = _.cloneDeep(questions)
+            questionClone = questions.filter(question => question.id !== id)
+            setQuestions(questionClone)
+        }
+    }
+
+    const handleAddRemoveAnswer = (type, questionId, answerId) => {
+        let questionClone = _.cloneDeep(questions)
+        if (type === 'ADD') {
+            const newAnswer = {
+                id: uuidv4(),
+                description: '',
+                isCorrect: false
+            }
+
+            let index = questionClone.findIndex(item => item.id === questionId)
+            questionClone[index].answers.push(newAnswer)
+            setQuestions(questionClone)
+        }
+
+        if (type === 'REMOVE') {
+            let index = questionClone.findIndex(item => item.id === questionId)
+            questionClone[index].answers = questionClone[index].answers.filter(item => item.id !== answerId)
+            setQuestions(questionClone)
+        }
+    }
+
+    const [questions, setQuestions] = useState(
+        [
+            {
+                id: uuidv4(),
+                description: 'question 1',
+                image: '',
+                imageFile: '',
+                imageName: '',
+                answers: [
+                    {
+                        id: uuidv4(),
+                        description: 'answer 1',
+                        isCorrect: false
+                    }
+                ]
+            }
+        ]
+    )
+
+    console.log('>>>> questions: ', questions)
     return (
         <div className='questions-container'>
             <div className='title'>
                 Manage Questions
             </div>
+            <hr />
             <div className='add-new-question'>
                 <div className='col-6 form-group'>
-                    <label>Select Quiz</label>
+                    <label className='mb-2'>Select Quiz</label>
                     <Select
                         value={selectedQuiz}
                         onChange={setSelectedQuiz}
                         options={options}
                     />
                 </div>
-                <div className='mt-3'>
+                <div className='mt-3 mb-2'>
                     Add questions:
                 </div>
-                <div>
-                    <div className='questions-content'>
-                        <div className="form-floating description">
-                            <input type="text" className="form-control" placeholder="name@example.com" />
-                            <label>Description</label>
-                        </div>
-                        <div className='group-upload'>
-                            <label className='label-up'>Upload Image</label>
-                            <input type={'file'} hidden />
-                            <span>0 file is uploaded</span>
-                        </div>
-                        <div className='btn-add'>
-                            <span>
-                                <FaPlusCircle className='icon-add' />
-                            </span>
-                            <span>
-                                <FaMinusCircle className='icon-remove' />
-                            </span>
-                        </div>
-                    </div>
-                    <div className='answers-content'>
-                        <input
-                            className="form-check-input iscorrect"
-                            type="checkbox"
-                        />
-                        <div className="form-floating answer-name">
-                            <input type="text" className="form-control" placeholder="name@example.com" />
-                            <label>Answers 1</label>
-                        </div>
-                        <div className='btn-group'>
-                            <span>
-                                <FaRegMinusSquare className='icon-add' />
-                            </span>
-                            <span>
-                                <FaRegPlusSquare className='icon-remove' />
-                            </span>
-                        </div>
-                    </div>
-                </div>
+                {
+                    questions && questions.length > 0 &&
+                    questions.map((question, index) => {
+                        return (
+                            <div key={question.id} className='q-main mb-4'>
+                                <div className='questions-content'>
+                                    <div className="form-floating description">
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            placeholder="name@example.com"
+                                            value={question.description}
+                                        />
+                                        <label>Question {index + 1} 's description</label>
+                                    </div>
+                                    <div className='group-upload'>
+                                        <label>
+                                            <RiImageAddFill className='label-up' />
+                                        </label>
+                                        <input type={'file'} hidden />
+                                        <span>0 file is uploaded</span>
+                                    </div>
+                                    <div className='btn-add'>
+                                        <span onClick={() => handleAddRemoveQuestion('ADD', '')}>
+                                            <FaPlusCircle className='icon-add' />
+                                        </span>
+                                        {questions.length > 1 &&
+                                            <span onClick={() => handleAddRemoveQuestion('REMOVE', question.id)}>
+                                                <FaMinusCircle className='icon-remove' />
+                                            </span>
+                                        }
+
+                                    </div>
+                                </div>
+                                {question && question.answers.length > 0 &&
+                                    question.answers.map((answer, index) => {
+                                        return (
+                                            <div key={answer.id} className='answers-content'>
+                                                <input
+                                                    className="form-check-input iscorrect"
+                                                    type="checkbox"
+                                                />
+                                                <div className="form-floating answer-name">
+                                                    <input
+                                                        value={answer.description}
+                                                        type="text"
+                                                        className="form-control"
+                                                        placeholder="name@example.com"
+                                                    />
+                                                    <label>Answers {index + 1}</label>
+                                                </div>
+                                                <div className='btn-group'>
+                                                    <span onClick={() => handleAddRemoveAnswer('ADD', question.id)}>
+                                                        <FaRegPlusSquare className='icon-add' />
+                                                    </span>
+                                                    {question.answers.length > 1 &&
+                                                        <span onClick={() => handleAddRemoveAnswer('REMOVE', question.id, answer.id)}>
+                                                            <FaRegMinusSquare className='icon-remove' />
+                                                        </span>
+                                                    }
+                                                </div>
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </div>
+                        )
+                    })
+                }
             </div>
         </div>
     )
