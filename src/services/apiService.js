da3a138102d2900245087b388035e6460f3a297f
Bach Dao <ngocbach12a1@gmail.com>
2025-01-10 12:04:35 +0700

    Hiển Thị Danh Sách Bài Thi Admin

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -69,9 +69,13 @@ const postCreateNewQuiz = (description, name, difficulty, quizImage) => {
     return axios.post('/api/v1/quiz', data)
 }
 
+const getAllQuizForAdmin = () => {
+    return axios.get(`/api/v1/quiz/all`)
+}
+
 export {
     postCreateNewUser, getAllUsers, putUpdateUser,
     deleteUser, getUserWithPaginate, postLogin,
     postRegister, getQuizByUser, getDataQuiz,
-    postSubmitQuiz, postCreateNewQuiz
+    postSubmitQuiz, postCreateNewQuiz, getAllQuizForAdmin
 }
