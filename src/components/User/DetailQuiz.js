8c09e1d98e3268eae34a80fe8d72aba4fc12777f
Bach Dao <ngocbach12a1@gmail.com>
2025-01-12 15:04:43 +0700

    Design base CSS question right-content

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -5,6 +5,7 @@ import _ from 'lodash'
 import './DetailQuiz.scss'
 import Question from './Question';
 import ModalResult from './ModalResult';
+import RightContent from './Content/RightContent';
 
 const DetailQuiz = (props) => {
     const params = useParams()
@@ -156,7 +157,9 @@ const DetailQuiz = (props) => {
                 </div>
             </div>
             <div className='right-content'>
-                count down
+                <RightContent
+                    dataQuiz={dataQuiz}
+                />
             </div>
             <ModalResult
                 show={isShowModalResult}
--- src/components/User/Question.js
+++ src/components/User/Question.js
@@ -1,7 +1,12 @@
 import _ from 'lodash'
+import { useState } from 'react';
+import Lightbox from "react-awesome-lightbox";
 
 const Question = (props) => {
     const { data, index } = props
+
+    const [isPreviewImage, setIsPreviewImage] = useState(false)
+
     if (_.isEmpty(data)) {
         return (<></>)
     }
@@ -15,12 +20,22 @@ const Question = (props) => {
         <>
             {data.image ?
                 <div className='q-image'>
-                    <img src={`data:image/jpeg;base64,${data.image}`} />
+                    <img
+                        src={`data:image/jpeg;base64,${data.image}`}
+                        style={{ cursor: 'pointer' }}
+                        onClick={() => setIsPreviewImage(true)}
+                    />
+                    {isPreviewImage === true &&
+                        <Lightbox
+                            image={`data:image/jpeg;base64,${data.image}`}
+                            title={'Question Image'}
+                            onClose={() => setIsPreviewImage(false)}
+                        >
+                        </Lightbox>
+                    }
                 </div>
                 :
-                <div className='q-image'>
-
-                </div>
+                <div className='q-image'></div>
             }
             <div className='question'>Questions {index + 1}: {data.questionDescription} ?</div>
             <div className='answer'>
