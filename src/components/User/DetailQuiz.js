d4234c0cb97b816e554e499321f8e60de4cdca22
Bach Dao <ngocbach12a1@gmail.com>
2025-01-12 16:14:50 +0700

    Countdown Timer

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -159,6 +159,7 @@ const DetailQuiz = (props) => {
             <div className='right-content'>
                 <RightContent
                     dataQuiz={dataQuiz}
+                    handleFinishQuiz={handleFinishQuiz}
                 />
             </div>
             <ModalResult
